41e2635dd0087a53def1879487e9e1a1b9697baf
Shrivara <shrivara.jois@gmail.com>
2017-10-31 12:58:29 +0530

    Remove console output logging

--- init.sh
+++ init.sh
@@ -5,45 +5,22 @@ readonly JOB_STATUS_PATH="$STATUS_DIR"/job.status
 readonly JOB_ENV_PATH="$STATUS_DIR"/job.env
 
 #
-# Headers
-#
-exec_cmd() {
-  cmd=$@
-  cmd_uuid=$(cat /proc/sys/kernel/random/uuid)
-  cmd_start_timestamp=`date +"%s"`
-  echo "__SH__CMD__START__|{\"type\":\"cmd\",\"sequenceNumber\":\"$cmd_start_timestamp\",\"id\":\"$cmd_uuid\"}|$cmd"
-  eval $cmd
-  cmd_status=$?
-  cmd_end_timestamp=`date +"%s"`
-  echo "__SH__CMD__END__|{\"type\":\"cmd\",\"sequenceNumber\":\"$cmd_start_timestamp\",\"id\":\"$cmd_uuid\",\"completed\":\"$cmd_status\"}|$cmd"
-  return $cmd_status
-}
-
-exec_grp() {
-  group_name=$1
-  group_uuid=$(cat /proc/sys/kernel/random/uuid)
-  group_start_timestamp=`date +"%s"`
-  echo "__SH__GROUP__START__|{\"type\":\"grp\",\"sequenceNumber\":\"$group_start_timestamp\",\"id\":\"$group_uuid\"}|$group_name"
-  eval "$group_name"
-  group_status=$?
-  group_end_timestamp=`date +"%s"`
-  echo "__SH__GROUP__END__|{\"type\":\"grp\",\"sequenceNumber\":\"$group_end_timestamp\",\"id\":\"$group_uuid\",\"completed\":\"$group_status\"}|$group_name"
-}
-
-#
 # Kick flow
 #
 set_status() {
+  echo "Setting status to success"
   echo "4002" > "$JOB_STATUS_PATH"
 }
 
 back_to_reqproc() {
+  echo "Handing control back to reqProc"
   echo "reqProc" > "$JOB_WHO_PATH"
 }
 
 kick() {
-  exec_cmd "set_status"
-  exec_cmd "back_to_reqproc"
+  echo "Kick starting the build"
+  set_status
+  back_to_reqproc
 }
 
 poll() {
@@ -54,7 +31,7 @@ poll() {
     else
       local who=$(cat $JOB_WHO_PATH)
       if [ "$who" = "reqKick" ]; then
-        consoles=$(exec_grp "kick")
+        kick
       else
         echo "Found $who in $JOB_WHO_PATH, skipping."
       fi
